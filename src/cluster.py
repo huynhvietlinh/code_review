06449c37e69598650ea9029cdf719289275b13b5
huynhvietlinh <vietlinh.huynh@gmail.com>
2021-02-09 23:13:34 -0500

    Add files via upload

--- src/cluster.py
+++ src/cluster.py
@@ -2,78 +2,76 @@ import csv
 import pandas as pd
 import numpy as np
 #
-def find_target(regs, ext_cut_off):
-
-def cluster_reg_element(regs, loops, ext_cut_off):
+def find_target(reg_ids, interactions, ext_cut_off):
   """
-  Function: Cluster regulatory elements (enhancers, promoters) by 
+  Function:
+  Inputs:
+    reg_ids:
+    interactions:
+    ext_cut_off: a threshold value (number of base pairs)
+  Output:
+    a list of ids
+  """
+  targets = []
+  return targets
+#
+def find_interactions(regs, loops, max_ext_len = 1e6):
+  """
+  Function: find possible interactions between regulatory elements (enhancers, promoters) by 
             the structural constraints of chromatin loops
   Inputs:
     regs: a dataframe, each row represents a regulatory element, 
           3 columns "chr", "start", "end" represent the coordinate
     loops: a dataframe, each row represents a loop, 
            3 columns "chr", "start", "end" represent the coordinate
-    ext_cut_off: a threshold value (number of base pairs)
+    max_ext_len: a threshold value (number of base pairs) 
   Output:
-    a list of lists, each list contains row ids of the input 
-    dataframe regs to represent a cluster of regulatory elements 
-    of these row ids
+    a dataframe, each row represents a possible interaction,
+    2 columns "row_1", "row_2" are row indexes of two regulatory 
+    elements in the input dataframe regs and column "min_ext_len" is the 
+    minimum extension length so that these two elements can interact
   """
+  
   regs["origin_index"] = regs.index
-  clusters = []
+  interaction_list = []
   for chr in regs.chr.unique():
     #print(chr)
-    chr_spec_regs = regs[(regs['chr'] == chr)]
-    chr_spec_regs.reset_index(inplace=True)
-    chr_spec_loops = loops[(loops['chr'] == chr)]
-    # 100kb only appears once here
-    chr_spec_shortest_ext = find_shortest_ext_len(chr_spec_regs, chr_spec_loops, 100000)
-    #print (chr_spec_shortest_ext)
-  return cluster
-def find_shortest_ext_len(regs, loops, max_ext_len):
-  """
-  Function: Find the shortest extension length so that a pair of 
-            regulatory elements can interact  
-  Inputs:
-    regs: a dataframe, the column "start" represent the position 
-          of a regulatory element represented by a row
-    loops: a dataframe, two columns "start" and "end" represent 
-           the coordinate of a loop represented by a row
-    max_extension: a threshold value (number of base pairs) 
-  Output:
-    a dataframe, 3 columns "row_1", "row_2", "shortest_ext_len"
-  
-  """
-  reg_num = len(regs.index)
-  min_loop_in = np.full((reg_num, reg_num), 1e7)
-  max_loop_out = np.zeros((reg_num, reg_num))
-  # add
-  for loop_index,loop in loops.iterrows():
-    for i, reg_i in regs.iterrows():
-      for j, reg_j in regs.iterrows():
-        if (i > j):
-          min_loop_in[i][j] = min(min_loop_in[i][j], 
-                                  find_loop_in_extension(loop['start'], loop['end'], 
-                                                         reg_i['start'], reg_j['start']))
-  # remove
-  for loop_index,loop in loops.iterrows():
-    tmp = []
-    for i,reg_i in regs.iterrows():
-      for j,reg_j in regs.iterrows():
-        if (i > j):
-          max_loop_out[i][j] = max(max_loop_out[i][j],
-                                   find_loop_out_extension(loop['start'], loop['end'], 
-                                                           reg_i['start'], reg_j['start']))
-  # combine
-  tmp = []
-  for j in range(reg_num):
-    for i in range(j + 1, reg_num):
-      shortest_ext_len = max(min_loop_in[i][j], max_loop_out[i][j])
-      if (shortest_ext_len <= max_ext_len):
-        tmp.append([i, j, shortest_ext_len])
-  return pd.DataFrame.from_records(tmp, columns=["row_1", "row_2", "shortest_ext_len"])
+    chr_loops = loops[(loops["chr"] == chr)]
+    chr_regs = regs[(regs["chr"] == chr)]
+    # sort "start" increasingly
+    chr_regs = chr_regs.sort_values("start")
+    chr_regs.reset_index(drop=True, inplace=True)
+    chr_reg_num = len(chr_regs.index)
+    min_loop_in = np.full((chr_reg_num, chr_reg_num), 1e7)
+    max_loop_out = np.zeros((chr_reg_num, chr_reg_num))
+    # elements in a loop can interact
+    for loop_index,loop in chr_loops.iterrows():
+      for i, reg_i in chr_regs.iterrows():
+        for j, reg_j in chr_regs.iterrows():
+          if (i < j):
+            min_loop_in[i][j] = min(min_loop_in[i][j], 
+                                    find_loop_in_extension(loop["start"], loop["end"], 
+                                                           reg_i["start"], reg_j["start"]))
+    # elements separated by a loop can not interact
+    for loop_index,loop in chr_loops.iterrows():
+      for i,reg_i in chr_regs.iterrows():
+        for j,reg_j in chr_regs.iterrows():
+          if (i < j):
+            max_loop_out[i][j] = max(max_loop_out[i][j],
+                                     find_loop_out_extension(loop["start"], loop["end"], 
+                                                             reg_i["start"], reg_j["start"]))
+    # combine
+    for i in range(chr_reg_num):
+      for j in range(i + 1, chr_reg_num):
+        shortest_ext_len = max(min_loop_in[i][j], max_loop_out[i][j])
+        if (shortest_ext_len <= max_ext_len):
+          interaction_list.append([chr_regs.iloc[i]["origin_index"], chr_regs.iloc[j]["origin_index"], 
+                                   shortest_ext_len])
+  #
+  return pd.DataFrame.from_records(interaction_list, columns=["row_1", "row_2", "shortest_ext_len"])
+#
 
-# 
+
 def find_loop_in_extension(a1, a2, e1, e2):
   if (e1 >= a2):
     return e2 - a2
@@ -89,12 +87,44 @@ def find_loop_in_extension(a1, a2, e1, e2):
       return a1 - e1
     else:
       return max(a1 - e1, e2 - a2)
-#
 def find_loop_out_extension(a1, a2, e1, e2):
   if (e1 >= a1 and e1 <= a2 and e2 > a2):
     return e2 - a2
   elif (e1 < a1 and e2 >= a1 and e2 <= a2):
     return a1 - e1
+  else:
+    return 0
+
+ 
+def find_loop_in_extension(LS, LE, e1, e2):
+  """
+  LS, LE: loop start and loop end
+  e1, e2: start position of two elements, e1 <= e2
+  """
+  if (e1 >= LE):
+    return e2 - LE #---LS---LE---e1---e2----
+  elif (e1 >= LS):
+    if (e2 >= LE):
+      return e2 - LE #---LS---e1---LE---e2----
+    else:
+      return 0 #---LS----e1--e2----LE----
+  else:
+    if (e2 <= LS):
+      return LS - e1 #---e1---e2---LS----LE----
+    elif (e2 <= LE):
+      return LS - e1 #---e1---LS----e2----LE----
+    else:
+      return max(LS - e1, e2 - LE) #---e1---LS---LE----e2-----
+#
+def find_loop_out_extension(LS, LE, e1, e2):
+  """
+  LS, LE: loop start and loop end
+  e1, e2: start position of two elements, e1 <= e2
+  """
+  if (e1 >= LS and e1 <= LE and e2 > LE):
+    return e2 - LE #---LS---e1----LE----e2----
+  elif (e1 < LS and e2 >= LS and e2 <= LE):
+    return LS - e1 #---e1----LS----e2----LE---
   else:
     return 0
 
@@ -105,11 +135,11 @@ def test(test_num):
   loop_df["start"] = round((loop_df["start1"] + loop_df["end1"])/2)
   loop_df["end"] = round((loop_df["start2"] + loop_df["end2"])/2)
   loops = loop_df[["chr", "start", "end"]]
-  print(loops)
+  #print(loops)
   # simple test
   if (test_num >= 1):
     reg_short_list_df = pd.read_csv("../data/2021_01_14_E_P_mESC/Jennifer_list.csv", sep = '\t')
-    print(cluster_reg_element(reg_short_list_df, loops, 5000))
+    print(find_interactions(reg_short_list_df, loops))
 
   # long test
   if (test_num >= 2):
@@ -126,6 +156,6 @@ def test(test_num):
     #print(reg_element_long_list)
     #reg_element_long_list = reg_element_long_list[(reg_element_long_list['chr'] == 'chr10')]
     reg_long_list_df.reset_index(drop=True, inplace=True)
-    print(cluster_reg_element(reg_short_list_df, loops, 5000))
+    print(find_interactions(reg_short_list_df, loops))
 ##
 test(1)
