c950d428509b1158a644f28822e675654c89cb0f
huynhvietlinh <vietlinh.huynh@gmail.com>
2022-09-19 15:02:22 +0700

    Create golden_spiral.py

---
+++ golden_spiral.py
@@ -0,0 +1,25 @@
+def draw_golden_spiral (n):
+  # every iteration turns 90 degrees
+  angles = [0, 270, 180, 90]
+  # this describes the direction 
+  vectors = [(-1,0), (0,1), (1,0), (0,-1)]
+  #
+  f = [0 for i in range(n+1)]
+  f[0] = 1
+  f[1] = 1
+  arc_origin = (0,0)
+  for i in range(1,n):
+    plt.gca().add_patch(Arc(
+      xy = arc_origin,
+      width = 2*f[i], height = 2*f[i],
+      angle = angles[i%len(angles)],
+      theta1 = 0, theta2 = 90, lw = 2    
+    ))
+    arc_origin = (arc_origin[0] + 
+                 vectors[i%len(vectors)][0]*f[i-1], 
+                 arc_origin[1] + 
+                 vectors[i%len(vectors)][1]*f[i-1])
+    f[i+1] = f[i] + f[i-1]  
+  plt.axis([-f[n], f[n], -f[n], f[n]])			
+  plt.show()
+
